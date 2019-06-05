b5dc5b554b7c361cc7dd48bc3dada7571f6c1cb3
davorin <47928470+davorinpiljic@users.noreply.github.com>
2019-06-05 15:52:05 -0600

    navigation 100%

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import './App.css';
 import { Home, About, Contact, Navigation } from './components';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
 
 const App = () => (
   <div>
     <Navigation />
+
+    <Route exact path="/"></Route>
+    <Route path="/about"></Route>
+    <Route path="/contact"></Route>    
   </div>
 );
 
